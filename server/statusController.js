62962217c82e2700adcdadb177256ed5d7980586
charles-spinosa <charles.spinosa@gmail.com>
2019-06-18 14:42:28 -0700

    [ feat ] functional API routes except for job enqueueing

--- server/statusController.js
+++ server/statusController.js
@@ -1,7 +1,29 @@
+const mongoHelpers = require('../db/mongoHelpers.js');
+
 module.exports = {
   enqueueJob: (req, res) => {
     //redisJobQueue.push(mongo.find(req.params.jobID))
   },
-  jobStatus: (req, res) => {},
-  jobResult: (req, res) => {}
+  jobStatus: (req, res) => {
+    mongoHelpers
+      .readOne(req.params.jobID)
+      .then(data => {
+        res.status(200).send(data.status);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send('failed to retrieve job status');
+      });
+  },
+  jobResult: (req, res) => {
+    mongoHelpers
+      .readOne(req.params.jobID)
+      .then(data => {
+        res.status(200).send(data.largestImage);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send('failed to retrieve job result');
+      });
+  }
 };
