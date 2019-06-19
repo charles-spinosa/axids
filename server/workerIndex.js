7662b14e70bfe66b705b87dc3534900eb01dba61
charles-spinosa <charles.spinosa@gmail.com>
2019-06-18 17:29:40 -0700

    [ dev ] pre-URL parsing commit to prep for branching

--- server/workerIndex.js
+++ server/workerIndex.js
@@ -1,6 +1,10 @@
 const Queue = require('bull');
 const jobsQueue = new Queue('jobsQueue');
 
+const Cheerio = require('cheerio');
+
+const grabImages = url => {};
+
 jobsQueue.process(async (job, done) => {
   console.log('hello from worker\n\n', job.data.job);
   done();
