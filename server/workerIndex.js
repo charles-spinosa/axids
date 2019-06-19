6f7065b6b47efdefa752ca1c68cedad6142601cd
charles-spinosa <charles.spinosa@gmail.com>
2019-06-18 20:48:40 -0700

    [ feat ] generate array of img src

--- server/workerIndex.js
+++ server/workerIndex.js
@@ -3,18 +3,40 @@ const jobsQueue = new Queue('jobsQueue');
 const request = require('request-promise-native');
 const Cheerio = require('cheerio');
 
-const grabImages = url => {
+const grabImageURLs = url => {
+  let baseURL = '';
+  let idxOfSlash = -1;
+  for (let i = 0; i < 3; i++) {
+    idxOfSlash = url.indexOf('/', idxOfSlash + 1);
+  }
+  baseURL = url.substring(0, idxOfSlash);
   return request(url).then(data => {
     $ = Cheerio.load(data);
-    $ = $('img');
-    console.log($);
-    return $;
+    tags = $('img');
+    // console.log(tags);
+    let URLs = [];
+    for (let key in tags) {
+      if (tags[key].type === 'tag' && tags[key].name === 'img') {
+        if (tags[key].attribs.src.includes('//')) {
+          URLs.push('http:' + tags[key].attribs.src);
+        } else if (!tags[key].attribs.src.includes('https')) {
+          URLs.push(baseURL + tags[key].attribs.src);
+        } else {
+          URLs.push(tags[key].attribs.src);
+        }
+      }
+    }
+    console.log(URLs);
+    return URLs;
   });
 };
 
 jobsQueue.process(async (job, done) => {
-  console.log('hello from worker', job.data.job);
-  grabImages(job.data.job.url)
+  console.log(
+    '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',
+    job.data.job
+  );
+  grabImageURLs(job.data.job.url)
     .then($ => done())
     .catch(err => console.log(err));
 });
