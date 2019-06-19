2891937df286636a9ccfcbaf05950bb0433ec261
charles-spinosa <charles.spinosa@gmail.com>
2019-06-18 18:01:49 -0700

    [ prog ] initial implementation of cheerio tag selection

--- server/workerIndex.js
+++ server/workerIndex.js
@@ -1,11 +1,20 @@
 const Queue = require('bull');
 const jobsQueue = new Queue('jobsQueue');
-
+const request = require('request-promise-native');
 const Cheerio = require('cheerio');
 
-const grabImages = url => {};
+const grabImages = url => {
+  return request(url).then(data => {
+    $ = Cheerio.load(data);
+    $ = $('img');
+    console.log($);
+    return $;
+  });
+};
 
 jobsQueue.process(async (job, done) => {
-  console.log('hello from worker\n\n', job.data.job);
-  done();
+  console.log('hello from worker', job.data.job);
+  grabImages(job.data.job.url)
+    .then($ => done())
+    .catch(err => console.log(err));
 });
